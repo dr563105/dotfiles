9e6ebcc561f240e0ed9f75c91d32b037290d481d
Deepak Ramani <dr563105@gmail.com>
2022-03-02 00:04:37 +0530

    added setting to display folders mentioned in gitignore

--- nvim/.config/nvim/lua/deepak/nvim-tree.lua
+++ nvim/.config/nvim/lua/deepak/nvim-tree.lua
@@ -2,7 +2,8 @@ vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_highlight_opened_files = 1
 require('nvim-tree').setup {
     auto_close = true,
-    view = {auto_resize = true}
+    view = {auto_resize = true},
+    git = {ignore = false} -- displays folders mentioned inside gitignore
 }
 -- vim.cmd('nnoremap <leader>e :NvimTreeToggle<CR>')
 vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', {noremap = true})
