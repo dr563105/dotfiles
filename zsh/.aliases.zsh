36ec7dc5e547efce6ef20779aae8994adf808667
Deepak Ramani <dr563105@gmail.com>
2022-08-27 14:00:05 +0530

    august month update

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -58,6 +58,15 @@ alias jn='jupyter notebook'
 alias nvd='nvidia-smi'
 alias nvdw='watch -n 1 nvidia-smi'
 
+# for prefect
+alias pstart='prefect orion start'
+alias pconfig='prefect config view'
+
+# for pipenv
+alias ppi='pipenv install'
+alias pps='pipenv shell'
+
+
 # for mac
 alias showfiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hidefiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
